0a791ff6938e159613a33eebaae5e4583e8db531
Juan Pablo Montoya <juan.montoya@ucb.edu.bo>
2024-09-22 14:19:28 -0400

    Pagina Incio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
+import { InicioComponent } from './paginas/inicio/inicio.component'; // Componente inicio 
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'inicio', component: InicioComponent },
+    { path: '**', component: InicioComponent },
+];
