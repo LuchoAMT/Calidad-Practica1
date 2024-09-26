b4296e90a6ea5a0ae64f5f219201014d6f201c97
Carlos Javier Sejas Pareja <carlos.sejas@ucb.edu.bo>
2024-09-25 21:00:03 -0400

    pagina de impresa implementada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes,RouterModule } from '@angular/router';
-import { InicioComponent } from './paginas/inicio/inicio.component'; // Componente inicio 
+import { InicioComponent } from './paginas/inicio/inicio.component'; // Componente inicio
 import { ListaProductosComponent } from './paginas/lista-productos/lista-productos.component';
+import { ListaEmpresasComponent } from './paginas/lista-empresas/lista-empresas.component';
 
 
 export const routes: Routes = [
     { path: 'inicio', component: InicioComponent },
     { path: 'productos', component: ListaProductosComponent },
+    { path: 'empresas', component: ListaEmpresasComponent },
     { path: '**', component: InicioComponent },
 ];
---
+++ src/app/paginas/lista-empresas/lista-empresas.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+import { EmpresasService } from '../../servicios/empresas.service';
+import { Empresa } from '../../interfaces/empresa';
+
+@Component({
+  selector: 'app-lista-empresas',
+  standalone: true,
+  imports: [MatCardModule, RouterLink],
+  templateUrl: './lista-empresas.component.html',
+  styleUrl: './lista-empresas.component.scss'
+})
+export class ListaEmpresasComponent implements OnInit {
+  empresas: Empresa[] = [];
+
+  constructor(private empresaService: EmpresasService) {}
+
+  async obtenerEmpresas() {
+    this.empresas = await this.empresaService.getEmpresas();
+  }
+
+  ngOnInit(): void {
+    this.obtenerEmpresas();
+  }
+}
+
