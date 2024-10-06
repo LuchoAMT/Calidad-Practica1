b07d39c20ffb2e4d2d49ffcb0ac272c3a50d257e
Luis Menacho <luisantonio.menacho@gmail.com>
2024-10-06 17:20:29 -0400

    needed commit

--- API/db.js
+++ API/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'PacMob_159_mj-21',
+    password: 'chulo02',
     database: 'merka2'
 });
 
