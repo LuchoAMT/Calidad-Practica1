29f601085f6ad7cd814afcecda840f207b57d275
Luis Menacho <luisantonio.menacho@gmail.com>
2024-10-10 12:23:04 -0400

    nueva bd

--- API/db.js
+++ API/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'sql3.freemysqlhosting.net',
-    user: 'sql3735209',
-    password: 'iYPaC2ZRGf',
-    database: 'sql3735209',
+    host: 'sql5.freesqldatabase.com',
+    user: 'sql5736832',
+    password: 'GESwtynrVD',
+    database: 'sql5736832',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
